75d072680029aa8bbecd2cb0a207e192d9909e77
ydarznek <ydarznek@gmail.com>
2023-11-15 19:52:58 +0300

    01_lab_tasks_1-3

---
+++ 01_lab/01_lab/1_Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Program.MyName();
+    }
+
+    private static void MyName()
+    {
+        string myName;
+
+        Console.WriteLine("Please enter your name");
+
+        myName = Console.ReadLine();
+
+        Console.WriteLine("Hello, {0}", myName);
+    }
+}
