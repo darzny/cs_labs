f592203c98cea2ddbe2cb9152263cbe41e7c5c11
ydarznek <ydarznek@gmail.com>
2023-11-16 21:35:57 +0300

    end 01_lab

---
+++ 01_lab/01_lab/2_Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Internal;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DivideIt();
+    }
+
+    private static void DivideIt()
+    {
+        try
+        {
+            Console.WriteLine("Please enter the first integer:");
+            if (!int.TryParse(Console.ReadLine(), out int i))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return;
+            }
+
+            Console.WriteLine("Please enter the second integer:");
+            if (!int.TryParse(Console.ReadLine(), out int j) || j == 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid non-zero integer.");
+                return;
+            }
+
+            int k = i / j;
+
+            string resultMessage = $"The result of dividing {i} by {j} is {k}";
+
+            Console.WriteLine(resultMessage);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("An exception was thrown: {0}", e.Message);
+        }
+    }
+}
---
+++ 01_lab/01_lab/3_Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Введите периметр равностороннего треугольника:");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double perimeter) && perimeter > 0)
+        {
+            double side = perimeter / 3;
+            double area = Math.Sqrt(3) / 4 * Math.Pow(side, 2);
+
+            Console.WriteLine("Сторона\tПлощадь");
+            Console.WriteLine($"{side:F2}\t{area:F2}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод");
+        }
+    }
+}
