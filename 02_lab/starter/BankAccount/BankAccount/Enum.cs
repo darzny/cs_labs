2f0555da7b2c8714ee39b61f644ecdae2ab9ad4f
ydarznek <ydarznek@gmail.com>
2023-11-17 13:21:35 +0300

    02_lab

---
+++ 02_lab/starter/BankAccount/BankAccount/Enum.cs
@@ -0,0 +1,16 @@
+﻿public enum AccountType { Checking, Deposit };
+
+internal class Program
+{
+    static void Main()
+    {
+        AccountType goldAccount;
+        AccountType platinumAccount;
+
+        goldAccount = AccountType.Checking;
+        platinumAccount = AccountType.Deposit;
+
+        Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+        Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+    }
+}
